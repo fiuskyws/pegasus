6dd0bea0b4c1c345aaa4456ffa665a75a994eeb8
Rafael Breno <rafael_breno@outlook.com>
2023-04-12 20:34:25 -0300

    feat: Add Validate method in Message

--- src/message/message.go
+++ src/message/message.go
@@ -1,10 +1,14 @@
 package message
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type (
 	// Message represents the data that's in the queue.
 	Message struct {
+		TopicName string `json:"topic_name"`
 		// Timestamp registers the time the message was Published
 		Timestamp *time.Time `json:"timestamp"`
 		// Attr is a map of message Attributes.
@@ -12,3 +16,14 @@ type (
 		Body []byte         `json:"body"`
 	}
 )
+
+const (
+	errEmptyField = "field '%s' is empty"
+)
+
+func (m *Message) Validate() error {
+	if m.TopicName == "" {
+		return fmt.Errorf(errEmptyField, "topic_name")
+	}
+	return nil
+}
